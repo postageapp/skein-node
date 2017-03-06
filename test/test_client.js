b09cb5cecf630b0c78e3eb2813477bed2007ee31
Scott Tadman <tadman@postageapp.com>
2017-03-06 17:06:15 -0500

    Better chaining init behaviour so that new Client().rpc() is viable without depending on two init promises

--- test/test_client.js
+++ test/test_client.js
@@ -9,5 +9,23 @@ const Client = require('../lib/client');
 // == Tests =================================================================
 
 describe('Client', () => {
+  it('can be created with defaults', () => {
+    var client = new Client();
 
+    return client.init.then(() => {
+      assert.ok(client);
+    })
+  });
+
+  describe('RPC', () => {
+    it('can create an RPC client', () => {
+      var client = new Client();
+
+      var rpc = client.rpc('test-exchange');
+
+      return rpc.init.then(() => {
+        assert.ok(rpc);  
+      });
+    });
+  });
 });
--- test/test_config.js
+++ test/test_config.js
@@ -29,7 +29,30 @@ describe('Config', () => {
 
     assert.equal(config.username, 'default_username');
     assert.equal(config.password, 'example_password');
+
     assert.equal(config.host, 'example_host');
+
+    assert.equal(config.uri(), 'amqp://default_username:example_password@example_host/');
+  });
+
+  it('can properly URI encode with a minimal config', () => {
+    var config = new Config({ });
+
+    assert.equal(config.uri(), 'amqp://localhost/');
+
+    assert.equal(config.driver, 'amqplib');
+  })
+
+  it('can properly URI encode values in configuration', () => {
+    var configPath = path.resolve(__dirname, './data/skein-encoding-example.json');
+
+    var config = new Config(configPath);
+
+    assert.ok(config);
+
+    assert.equal(config.uri(), 'amqp://User%20Name:%25%26pas%24word@example_host.example.net:65380/vhostN');
+
+    assert.equal(config.driver, 'amqplib');
   });
 
   it('has a configuration file it can read', () => {
